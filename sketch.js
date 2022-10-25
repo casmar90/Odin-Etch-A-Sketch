be4509c9a3d20dbef4d1e990327ac3b9eb50657e
Mariusz <casmar90@gmail.com>
2022-10-25 04:06:39 +0200

    Just started, grid design

--- sketch.js
+++ sketch.js
@@ -1,1 +1,9 @@
+for (let i = 0; i < 256; i++) {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    document.getElementById('container').appendChild(pixel);
+ }
+
+
+
 
