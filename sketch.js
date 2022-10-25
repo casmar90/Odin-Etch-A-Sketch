87fc202993677f060f15fd52a0f51c638dba39f2
Mariusz <casmar90@gmail.com>
2022-10-25 21:45:10 +0200

    Added buttons

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 for (let i = 0; i < 256; i++) {
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
-    document.getElementById('container').appendChild(pixel);
+    document.getElementById('grid').appendChild(pixel);
 }
 
 
