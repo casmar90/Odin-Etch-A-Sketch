b594659d08db98820a80f002740018ef9923826d
Mariusz <casmar90@gmail.com>
2022-10-27 15:47:04 +0200

    Grid size slider is working, drawing stopped :D

--- sketch.js
+++ sketch.js
@@ -1,107 +1,113 @@
-// Grid 
+ let gridSize = 16;
 
-let gridSize = 16;
+ //Grid size slider
+ let slider = document.getElementById("myRange");
+ let output = document.getElementById("demo");
+ output.innerHTML = slider.value; // Display the default slider value
 
-for (let i = 0; i < gridSize; i++) {
+ // Update the current slider value (each time you drag the slider handle)
+ slider.oninput = function () {
+   output.innerHTML = this.value;
+   // return this.value;
+   gridSize = this.value;
+   createGrid();
+ }
 
+
+ createGrid()
+function createGrid() {
+
+  const resetGrid = document.getElementById('grid');
+  resetGrid.innerHTML = '';
+
+  for (let i = 0; i < gridSize; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
     row.id = "row-" + (i + 1);
     document.getElementById('grid').appendChild(row);
-    
-}
+   }
 
-for (let i = 0; i < gridSize; i++) {
+   for (let i = 0; i < gridSize; i++) {
+     for (let i = 0; i < gridSize; i++) {
+       let pixel = document.createElement('div');
+       pixel.classList.add('pixel');
+       document.getElementById('row-' + (i + 1)).appendChild(pixel);
+     }
+   }
 
-    for (let i = 0; i < gridSize; i++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        document.getElementById('row-' + (i + 1)).appendChild(pixel);
-    }
-}
+ }
 
+ // Buttons
+ let selectedColor = 'red';
 
-// Grid size slider
-// var slider = document.getElementById("myRange");
-// var output = document.getElementById("demo");
-// output.innerHTML = slider.value; // Display the default slider value
+ const red = document.querySelector('.red');
+ red.onclick = () => selectedColor = ('red')
 
-// // Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function() {
-//   output.gridSize = this.value;
-// }
+ const green = document.querySelector('.green');
+ green.onclick = () => selectedColor = ('green');
 
-// Buttons
-let selectedColor = 'red';
+ const blue = document.querySelector('.blue');
+ blue.onclick = () => selectedColor = ('blue');
 
-const red = document.querySelector('.red');
-red.onclick = () => selectedColor = ('red')
+ const yellow = document.querySelector('.yellow');
+ yellow.onclick = () => selectedColor = ('yellow');
 
-const green = document.querySelector('.green');
-green.onclick = () => selectedColor = ('green');
+ const black = document.querySelector('.black');
+ black.onclick = () => selectedColor = ('black');
 
-const blue = document.querySelector('.blue');
-blue.onclick = () => selectedColor = ('blue');
-
-const yellow = document.querySelector('.yellow');
-yellow.onclick = () => selectedColor = ('yellow');
-
-const black = document.querySelector('.black');
-black.onclick = () => selectedColor = ('black');
-
-const white = document.querySelector('.white');
-white.onclick = () => selectedColor = ('white');
+ const white = document.querySelector('.white');
+ white.onclick = () => selectedColor = ('white');
 
 
-// let r = Math.floor(Math.random() * 255);
-// let g = Math.floor(Math.random() * 255);
-// let b = Math.floor(Math.random() * 255);
-// console.log(r);
-// console.log(g);
-// console.log(b);
+ // let r = Math.floor(Math.random() * 255);
+ // let g = Math.floor(Math.random() * 255);
+ // let b = Math.floor(Math.random() * 255);
+ // console.log(r);
+ // console.log(g);
+ // console.log(b);
 
-// function rgb(r, g, b){
-//     return "rgb("+r+","+g+","+b+")";
-//   }
-//   console.log(rgb (r, g, b));
+ // function rgb(r, g, b){
+ //     return "rgb("+r+","+g+","+b+")";
+ //   }
+ //   console.log(rgb (r, g, b));
 
-//   document.getElementById("myH2").style.color 
+ //   document.getElementById("myH2").style.color 
 
-//  let randomColor = rgb(155, 102, 102);
+ //  let randomColor = rgb(155, 102, 102);
 
 
 
 
-const rainbow = document.querySelector('.rainbow');
-rainbow.onclick = () => selectedColor = ('rainbow');
+ const rainbow = document.querySelector('.rainbow');
+ rainbow.onclick = () => selectedColor = ('rainbow');
 
 
 
-const clear = document.querySelector('.clear');
-let items = document.querySelectorAll('.pixel');
-clear.onclick = () => {
-    let items = document.querySelectorAll('.pixel');
-    items.forEach(item => item.className = 'pixel');
-}
+ const clear = document.querySelector('.clear');
+ let items = document.querySelectorAll('.pixel');
+ clear.onclick = () => {
+   let items = document.querySelectorAll('.pixel');
+   items.forEach(item => item.className = 'pixel');
+ }
 
 
 
 
 
 
-// Drawing
-drawing();
+ // Drawing
+ drawing();
 
-function drawing() {
-    let items = document.querySelectorAll('.pixel');
-    items.forEach(item => {
+ function drawing() {
+   let items = document.querySelectorAll('.pixel');
+   items.forEach(item => {
 
-        item.addEventListener('mouseover', () => {
-            item.className = 'pixel';
-            item.classList.add(selectedColor);
-        });
+     item.addEventListener('mouseover', () => {
+       item.className = 'pixel';
+       item.classList.add(selectedColor);
+     });
 
 
 
-    });
-}
+   });
+ }
