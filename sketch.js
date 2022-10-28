24b61d1d66225f1a0a7b6f891275e089c32c4bcc
Mariusz <casmar90@gmail.com>
2022-10-28 20:00:26 +0200

    just saving

--- sketch.js
+++ sketch.js
@@ -38,6 +38,24 @@
  }
 
  // BUTTONS
+
+
+ // Add active class to the current button (highlight it)
+//  let buttonsRow = document.getElementById('buttons');
+//  let btns = buttonsRow.getElementsByClassName('btn');
+
+//  for (let i = 0; i < btns.length; i++) {
+
+//   btns[i].addEventListener('click', function() {
+
+//   let current = document.getElementsByClassName('active');
+//   current[0].className = current[0].className.replace(' active', '');
+//   this.className += ' active';
+
+//   });
+//  }
+ 
+ 
  let selectedColor = 'red';
 
  const red = document.querySelector('.red');
@@ -46,61 +64,70 @@
  const yellow = document.querySelector('.yellow');
  const black = document.querySelector('.black');
  const eraser = document.querySelector('.eraser');
+ 
 
-red.addEventListener('mouseleave', function() {
-  this.classList.remove('buttonHover');
-});
 
-red.addEventListener('click', function() {
-  this.classList.add('buttonHover');
-});
-
-// red.addEventListener('mousedown', function() {
-//   this.classList.add('active');
+// red.addEventListener('mouseleave', function() {
+//   this.classList.remove('buttonHover');
+//  });
+ 
+// red.addEventListener('click', function() {
+//  this.classList.add('buttonHover');
 // });
 
+
+// red.addEventListener('mousedown', function() {
+  //   this.classList.add('active');
+  // });
+  
+red.onclick = () => selectedColor = ('red');
  green.onclick = () => selectedColor = ('green');
  blue.onclick = () => selectedColor = ('blue');
  yellow.onclick = () => selectedColor = ('yellow');
  black.onclick = () => selectedColor = ('black');
  eraser.onclick = () => selectedColor = ('eraser');
 
- //  let r = Math.floor(Math.random() * 255);
- //  let g = Math.floor(Math.random() * 255);
- //  let b = Math.floor(Math.random() * 255);
- //  console.log(r);
- //  console.log(g);
- //  console.log(b);
+ let r='',g='',b='';
+ function rgb(r, g, b) {
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+      return "rgb(" + r + "," + g + "," + b + ")";
+  }
 
- //  function rgb(r, g, b) {
- //      return "rgb(" + r + "," + g + "," + b + ")";
- //  }
- //  console.log(rgb(r, g, b));
- //  let randomColor = rgb(r, g, b);
+  // console.log(rgb(r, g, b));
+  let randomColor = rgb(r, g, b);
+
 
  //   document.getElementById("myH2").style.color 
 
  //  let randomColor = rgb(155, 102, 102);
 
- const rainbow = document.querySelector('.rainbow');
- rainbow.onclick = () => selectedColor = ('rainbow');
+const rainbow = document.querySelector('.rainbow');
+//  rainbow.onclick = () => selectedColor = ('rainbow');
 
- const clear = document.querySelector('.clear');
- let items = document.querySelectorAll('.pixel');
- clear.onclick = () => {
-     let items = document.querySelectorAll('.pixel');
-     items.forEach(item => item.className = 'pixel');
- }
+
+// rainbow.onclick = () => selectedColor = red.style.color = rgb(r, g, b);
+
+
+
+const clear = document.querySelector('.clear');
+let items = document.querySelectorAll('.pixel');
+clear.onclick = () => {
+  let items = document.querySelectorAll('.pixel');
+  items.forEach(item => item.className = 'pixel');
+}
+
 
  // DRAWING
  drawing();
-
- function drawing() {
-     let items = document.querySelectorAll('.pixel');
-     items.forEach(item => {
-         item.addEventListener('mouseover', () => {
-             item.className = 'pixel';
-             item.classList.add(selectedColor);
-         });
-     });
- }
+ 
+//  function drawing() {
+//   let items = document.querySelectorAll('.pixel');
+//   items.forEach(item => {
+//       item.addEventListener('mouseover', () => {
+//           item.className = 'pixel';
+//           item.classList.add(selectedColor);
+//       });
+//   });
+// }
